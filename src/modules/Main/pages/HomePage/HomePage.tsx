db09c5ce9c0e57cb81feb0d05790c383e87fd762
Danh <handoiditu1508@gmail.com>
2025-04-01 18:01:04 +0700

    home page

--- src/modules/Main/pages/HomePage/HomePage.tsx
+++ src/modules/Main/pages/HomePage/HomePage.tsx
@@ -1,6 +1,27 @@
+import { styled, Typography } from "@mui/material";
+
+const StyledTable = styled("table")(({ theme }) => ({
+  maxWidth: 400,
+  margin: `${theme.spacing(1)} auto 0`,
+  borderCollapse: "separate",
+  borderSpacing: theme.spacing(1),
+}));
+
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <StyledTable>
+      <tr>
+        <td><Typography>File Name.mp3</Typography></td>
+        <td><Typography variant="body2">5Mb</Typography></td>
+      </tr>
+      <tr>
+        <td><Typography>Song Name</Typography></td>
+        <td><Typography variant="body2">5:10</Typography></td>
+      </tr>
+      <tr>
+        <td><Typography>Artist 1, Artist 2, Artist 3, Artist 4, Artist 5, Artist 6, Artist 7</Typography></td>
+      </tr>
+    </StyledTable>
   );
 }
 
