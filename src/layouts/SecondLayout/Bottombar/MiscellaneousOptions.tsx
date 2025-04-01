809171f43fcdb5b8f1793ae50d34266cc71858fc
Danh <handoiditu1508@gmail.com>
2025-04-01 17:31:03 +0700

    bottom bar

---
+++ src/layouts/SecondLayout/Bottombar/MiscellaneousOptions.tsx
@@ -0,0 +1,62 @@
+import AutoModeIcon from "@mui/icons-material/AutoMode";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import VolumeMuteIcon from "@mui/icons-material/VolumeMute";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import { Box, Checkbox, IconButton, Tooltip } from "@mui/material";
+import { useState } from "react";
+
+const getVolumeIcon = (volume: number) => {
+  if (volume >= 100) {
+    return <VolumeUpIcon />;
+  }
+
+  if (volume > 50) {
+    return <VolumeDownIcon />;
+  }
+
+  if (volume > 0) {
+    return <VolumeMuteIcon />;
+  }
+
+  return <VolumeOffIcon />;
+};
+
+function MiscellaneousOptions() {
+  const [volume, setVolume] = useState<number>(100);
+  const volumeTitle = volume ? "Volume" : "Muted volume";
+  const volumeIcon = getVolumeIcon(100);
+
+  return (
+    <Box sx={{
+      flexGrow: 0,
+      flexShrink: 1,
+      flexBasis: 300,
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "flex-start",
+      gap: 1,
+    }}>
+      <Tooltip title="Auto play" placement="top">
+        <Checkbox
+          inputProps={{ "aria-label": "Auto play" }}
+          icon={<AutoModeIcon />}
+          checkedIcon={<AutoModeIcon />}
+        />
+      </Tooltip>
+      <Tooltip title={volumeTitle} placement="top">
+        <IconButton aria-label={volumeTitle}>
+          {volumeIcon}
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="More options" placement="top">
+        <IconButton aria-label="More options">
+          <MoreVertIcon />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+}
+
+export default MiscellaneousOptions;
